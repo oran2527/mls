c0718279aff57600099cb959406364d845e8126f
oran2527 <orlago250183@gmail.com>
2021-10-13 01:19:41 +0000

    navbar 2

--- mlsapp/urls.py
+++ mlsapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name='mslapp-index'),
+    path('', views.index, name='mlsapp-index'),
     path('predictions/', views.predictions, name='mlsapp-predictions')
 ]
