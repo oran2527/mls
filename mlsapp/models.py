3e32dd433d4a7d9216f7c90d62e4e1052929c301
oran2527 <orlago250183@gmail.com>
2021-10-23 15:30:49 +0000

    age validations model

--- mlsapp/models.py
+++ mlsapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -9,7 +10,12 @@ GENDER = (
 
 class Data(models.Model):
     name = models.CharField(max_length=100, null=True)
-    age = models.PositiveIntegerField(null=True)
+    age = models.PositiveIntegerField(
+        validators = [
+            MinValueValidator(8),
+            MaxValueValidator(17)
+        ],
+        null=True)
     height = models.PositiveIntegerField(null=True)
     sex = models.PositiveIntegerField(choices=GENDER, null=True)
     predictions = models.CharField(max_length=100, blank=True)
