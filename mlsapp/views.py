3e6ed4a2e78e196ec47f032515425593f94769eb
oran2527 <orlago250183@gmail.com>
2021-10-25 20:14:55 +0000

    views adding form information dictionary

--- mlsapp/views.py
+++ mlsapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .forms import DataForm
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'mlsapp/index.html')
+    form = DataForm()
+    context = {
+        'form': form, 
+    }
+    return render(request, 'mlsapp/index.html', context)
 
 def predictions(request):
     return render(request, 'mlsapp/predictions.html')
