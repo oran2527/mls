c84b9721b5f48e672429ebe5717749f9ccfa0f9e
oran2527 <orlago250183@gmail.com>
2021-10-25 19:26:30 +0000

    ml model

--- mlsapp/models.py
+++ mlsapp/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from sklearn.tree import DecisionTreeClassifier
+import joblib
+
 
 # Create your models here.
 
@@ -20,6 +23,11 @@ class Data(models.Model):
     sex = models.PositiveIntegerField(choices=GENDER, null=True)
     predictions = models.CharField(max_length=100, blank=True)
     date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        ml_model = joblib.load('ml_model/mls_sport_model.joblib')
+        self.predictions = ml_model.predict([[ self.age, self.height, self.sex ]])
+        return super().save(*args, *kwargs)
 
     class Meta:
         ordering = ['-date']
